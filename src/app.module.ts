497eec886f63c72793ce34feb221a2f4b47e2275
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-22 10:43:07 +0900

    feat: app.module.tsの項目追加
    feat: imports内にapolloを利用するための明示
    feat: providerを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { UserResolver } from './users/users.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserResolver],
 })
 export class AppModule {}
