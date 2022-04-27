2e2d053809a8ffdec746f21e5b933f914c75b94d
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-27 22:02:28 +0900

    fix: payloadの修正

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,5 +1,5 @@
 // import先が'passport-local'では無い事に注意！
-import { ExtractJwt, Strategy as BaseJwtStrategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
@@ -9,15 +9,14 @@ import { User } from 'src/users/models/user.model';
 
 // JwtについているPayload情報の型
 interface JWTPayload {
-  userId: User['id'];
-  username: User['name'];
+  id: number;
 }
 
 /**
  * @description JWTの認証処理を行うクラス
  */
 @Injectable()
-export class JwtStrategy extends PassportStrategy(BaseJwtStrategy) {
+export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
     super({
       // Authorization bearerからトークンを読み込む関数を返す
@@ -32,6 +31,6 @@ export class JwtStrategy extends PassportStrategy(BaseJwtStrategy) {
   // ここでPayloadを使ったバリデーション処理を実行できる
   // Payloadは、AuthService.login()で定義した値
   async validate(payload: JWTPayload): Promise<JwtPayload> {
-    return { userId: payload.userId, username: payload.username };
+    return { id: payload.id };
   }
 }
