d3e78d82d67109007962ac8a51319b2fb2bd146f
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-04-29 16:12:26 +0900

    feat: tagのseed用処理を作成

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient, User, Tag } from '@prisma/client';
 const prisma = new PrismaClient();
 
 // モデル投入用のデータ定義
@@ -23,6 +23,12 @@ const user_data: User[] = [
   },
 ];
 
+const tag_data: Tag[] = [
+  { id: 1, name: '勉強', user_id: 1 },
+  { id: 2, name: '仕事', user_id: 2 },
+  { id: 3, name: '趣味', user_id: 3 },
+];
+
 const seedingUser = async () => {
   const users = [];
   for (const user of user_data) {
@@ -34,12 +40,27 @@ const seedingUser = async () => {
   return await prisma.$transaction(users);
 };
 
+const seedingTag = async () => {
+  const tags = [];
+  for (const tag of tag_data) {
+    const create_tags = prisma.tag.create({
+      data: tag,
+    });
+    tags.push(create_tags);
+  }
+  return await prisma.$transaction(tags);
+};
+
 const main = async () => {
   console.log(`Start seeding ...`);
 
   console.log('Seeding User...');
   await seedingUser();
   console.log('Seeding User finished.');
+
+  console.log('Seeding Tag...');
+  await seedingTag();
+  console.log('Seeding Tag finished.');
 
   console.log(`Seeding finished.`);
 };
