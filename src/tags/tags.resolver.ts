b8629c1beb79ec34cbf6221b391a2a206eca84ed
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-05 12:22:23 +0900

    feat: タグ作成の処理を作成
    feat: createTagメソッドを作成
    feat: GraphQLのスキーマ更新

--- src/tags/tags.resolver.ts
+++ src/tags/tags.resolver.ts
@@ -1,8 +1,6 @@
 import { User } from '.prisma/client';
 import { UseGuards, Request, HttpException, HttpStatus } from '@nestjs/common';
 import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { UserAuth } from 'src/auth/dto/user-auth';
-import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CurrentUser, JwtAuthGuard } from 'src/auth/guards/jwt-guard';
 import { PrismaService } from 'src/prisma.service';
 import { Tag } from './models/tag.model';
@@ -34,6 +32,31 @@ export class TagResolver {
       throw new HttpException('そのタグは登録済みです', HttpStatus.CONFLICT);
     return this.prisma.tag.create({
       data: { name: trim_name, user_id: user.id },
+    });
+  }
+
+  @Mutation(() => Tag)
+  @UseGuards(JwtAuthGuard)
+  async updateTag(
+    @Args('id') id: number,
+    @Args('name') name: string,
+    @CurrentUser() user: User,
+  ) {
+    const registeredTag = await this.prisma.tag.findFirst({
+      where: { id: id },
+    });
+    if (registeredTag === null)
+      throw new HttpException('そのタグは存在しません', HttpStatus.NOT_FOUND);
+
+    const trim_name = name.trim();
+    if (trim_name === '')
+      throw new HttpException(
+        'タグ名を入力してください',
+        HttpStatus.BAD_REQUEST,
+      );
+    return this.prisma.tag.update({
+      where: { id: id },
+      data: { name: trim_name },
     });
   }
 }
