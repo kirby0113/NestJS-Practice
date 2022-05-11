2ebd93d3b0d3191f47d97f838c1befacd805ae9f
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-11 17:17:45 +0900

    fix: ビルドファイル削除

--- dist/tags/tags.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from '../prisma.service';
-import { Tag } from '@prisma/client';
-import { CreateTag } from './models/tag.model';
-export declare class TagService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    createTag(data: CreateTag): import(".prisma/client").Prisma.Prisma__TagClient<Tag>;
-}
