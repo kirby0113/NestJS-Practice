5b319a751da086e1485f3e23be88489a8340257b
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-11 22:40:44 +0900

    fix

--- dist/auth/dto/update-diary.input.d.ts
+++
@@ -1,7 +0,0 @@
-import { InputTag } from 'src/tags/models/tag.model';
-export declare class UpdateDiaryInput {
-    id: number;
-    title: string;
-    detail: string;
-    tags: InputTag[];
-}
--- dist/diaries/models/diary.model.d.ts
+++
@@ -1,8 +0,0 @@
-import { Tag } from 'src/tags/models/tag.model';
-export declare class Diary {
-    id: number;
-    title: string;
-    detail: string;
-    created_at: Date;
-    tags: Tag[];
-}
