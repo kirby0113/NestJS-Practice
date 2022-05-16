f0958f29c18f8f14657b40345b1ed4b31189d2dc
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-17 02:47:11 +0900

    fix: タグ周りの型修正

--- src/tags/models/tag.model.ts
+++ src/tags/models/tag.model.ts
@@ -4,20 +4,26 @@ import { ArgsType, Field, ID, InputType, ObjectType } from '@nestjs/graphql';
 export class Tag {
   @Field(() => ID)
   id: number;
+  @Field()
   name: string;
+  @Field()
   user_id: number;
 }
 
 @InputType()
 export class InputTag {
-  @Field()
+  @Field(() => ID)
   id: number;
+  @Field()
   name?: string;
+  @Field()
   user_id?: number;
 }
 
 @ObjectType()
 export class CreateTag {
+  @Field()
   name: string;
+  @Field()
   user_id: number;
 }
