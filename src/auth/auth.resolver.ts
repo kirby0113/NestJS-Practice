21d47531149ff76342cf586ecbb4d50868648a68
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-05-18 20:13:59 +0900

    fix: 新規登録の引数を修正

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -7,6 +7,7 @@ import { LoginResponse } from '../auth/dto/login-response';
 import { LoginUserInput } from '../auth/dto/login-user.input';
 import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
 import { PrismaService } from '../prisma.service';
+import { RegisterUserInput } from './dto/register-user.input';
 
 @Resolver()
 export class AuthResolver {
@@ -26,12 +27,10 @@ export class AuthResolver {
 
   @Mutation(() => LoginResponse)
   async register(
-    @Args('name') name: string,
-    @Args('email') email: string,
-    @Args('password') password: string,
+    @Args('registerUserInput') registerUserInput: RegisterUserInput,
   ) {
     const registered_user = await this.prisma.user.findUnique({
-      where: { email: email },
+      where: { email: registerUserInput.email },
     });
     if (registered_user) {
       throw new HttpException(
@@ -41,9 +40,11 @@ export class AuthResolver {
     }
     const user = await this.prisma.user.create({
       data: {
-        name,
-        email,
-        password: await bcrypt.hash(password, await bcrypt.genSalt(10)),
+        ...registerUserInput,
+        password: await bcrypt.hash(
+          registerUserInput.password,
+          await bcrypt.genSalt(10),
+        ),
       },
     });
     return this.authService.login(user);
